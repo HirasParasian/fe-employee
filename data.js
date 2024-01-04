709c41476ab8ff1bae2a29fbe988c260e3c2c57a
hiras.p <h.parasian@deltadatamandiri.com>
2024-01-04 10:56:12 +0700

    Feat : Update

--- data.js
+++ data.js
@@ -150,3 +150,109 @@ let DATAX = [
         ]
     }
 ]
+
+var xyz = [
+    {
+        "ProductID": 1,
+        "ProductName": "Chai",
+        "UnitPrice": 18,
+        "UnitsInStock": 39,
+        "Discontinued": false
+    },
+    {
+        "ProductID": 2,
+        "ProductName": "Chang",
+        "UnitPrice": 19,
+        "UnitsInStock": 17,
+        "Discontinued": false
+    },
+    {
+        "ProductID": 3,
+        "ProductName": "Aniseed Hiras",
+        "UnitPrice": 10,
+        "UnitsInStock": 13,
+        "Discontinued": false
+    },
+]
+
+let DATAX_FIX = [
+    {
+        loadingEqp : "CE6164 - 6020",
+        service : "OVERBURDEN",
+        operator : "YOHANES BENI WIJAYA",
+        foreman : 3714,
+        supervisor : 3714,
+        location : "PIT KGB",
+        material : "OVERBUDEN",
+        production : 329.22,
+        uom:"test",
+        data : [
+                {
+                    haulingEq : "CO4582 - 777",
+                    operator : "Syaifullah - 11",
+                    rit : "1",
+                    cap : "41.21",
+                    measurement:"82.66",
+                    measured:"0",
+                    dest:"OPD",
+                    distance:"2900",
+                    actMeas : "0",
+                    grade:"C"
+                },
+                {
+                    haulingEq : "CO4582 - 778",
+                    operator : "Wijaya Agung - 11",
+                    rit : "1",
+                    cap : "41.21",
+                    measurement:"82.66",
+                    measured:"0",
+                    dest:"OPD",
+                    distance:"2900",
+                    actMeas : "0",
+                    grade:"D"
+                }
+        ]
+    },
+    {
+        loadingEqp : "CE6164 - 6021",
+        service : "OVERBURDEN",
+        operator : "YOHANES BENI WIJAYA",
+        foreman : 3714,
+        supervisor : 3714,
+        location : "PIT KGB",
+        material : "OVERBUDEN",
+        production : 329.22,
+        uom:"test",
+        data : [
+                {
+                    haulingEq : "CO4582 - 779",
+                    operator : "Syaifullah - 11",
+                    rit : "1",
+                    cap : "41.21",
+                    measurement:"82.66",
+                    measured:"0",
+                    dest:"OPD",
+                    distance:"2900",
+                    actMeas : "0",
+                    grade:"A"
+                },
+                {
+                    haulingEq : "CO4582 - 775",
+                    operator : "Wijaya Agung - 11",
+                    rit : "1",
+                    cap : "41.21",
+                    measurement:"82.66",
+                    measured:"0",
+                    dest:"OPD",
+                    distance:"2900",
+                    actMeas : "0",
+                    grade:"B"
+                }
+        ]
+    }
+]
+
+let kkk = DATAX_FIX.filter(a=>{if(a.loadingEqp == "CE6164 - 6020"){
+    return a.data
+}})
+console.log(kkk)
